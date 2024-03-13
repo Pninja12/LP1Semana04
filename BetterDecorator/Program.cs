dff8126b30106c64b8cc2ba7493a774f44c99b27
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-13 11:28:55 +0000

    Ex1 concluído e testado

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="c"></param>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        private static string Decor (string s , char c ,int i)
+        {
+            string final = "";
+            for(int n = 0; n < i; n++)
+            {
+                final += $"{c}";
+            }
+            final += $" {s} ";
+            for(int n = 0; n < i; n++)
+            {
+                final += $"{c}";
+            }
+            return (final);
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Decor(args[0],char.Parse(args[1]),int.Parse(args[2])));
+        }
+    }
+}
