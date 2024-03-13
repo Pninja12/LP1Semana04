cd736bb3a59cf282c947b0ab57630483707cf7ab
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-13 11:48:08 +0000

    Ex1 devidamente comentado

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -5,20 +5,29 @@ namespace BetterDecorator
     class Program
     {
         /// <summary>
-        /// 
+        /// Função Decor e recebe uma string, um char e um int e envia o char
+        /// repetido um número int de vezes no fim e no início e no meio mete a
+        /// string
         /// </summary>
-        /// <param name="s"></param>
-        /// <param name="c"></param>
-        /// <param name="i"></param>
-        /// <returns></returns>
+        /// <param name="s">String recebida para imprimir no meio</param>
+        /// <param name="c">Char recebido para imprimir um número de vezes no
+        /// início e fim da string</param>
+        /// <param name="i">Int recebido para repetir o char</param>
+        /// <returns>Uma string</returns>
         private static string Decor (string s , char c ,int i)
         {
-            string final = "";
+            string final = "";//Criação da string a ser enviada no fim
+
+            //Loop para o inicio da string
             for(int n = 0; n < i; n++)
             {
                 final += $"{c}";
             }
+
+            //String do meio da string
             final += $" {s} ";
+
+            //Loop para o fim da string
             for(int n = 0; n < i; n++)
             {
                 final += $"{c}";
@@ -27,9 +36,16 @@ namespace BetterDecorator
             
         }
 
+        /// <summary>
+        /// Programa que recebe 3 parâmetros e envia para uma função para 
+        /// receber uma string e imprimi-la
+        /// </summary>
+        /// <param name="args">Recebe 3 parâmetro, 1 string, 1 char e 
+        /// um int</param>
         static void Main(string[] args)
         {
-            Console.WriteLine(Decor(args[0],char.Parse(args[1]),int.Parse(args[2])));
+            Console.WriteLine(Decor(args[0],char.Parse(args[1])
+            ,int.Parse(args[2])));
         }
     }
 }
