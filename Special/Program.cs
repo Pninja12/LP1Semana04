fed2a61cc146f7e79763b094916bfac9ffbf12d3
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-14 23:54:26 +0000

    Ex4 concluído e testado

---
+++ Special/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Special
+{
+    class Program
+    {
+        private static int GetSpecial(int n)
+        {
+            if (n <= 0)
+                return (-n);
+            else
+                return GetSpecial(n-1) + 2 * GetSpecial(n-2);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            Console.WriteLine(GetSpecial(int.Parse(Console.ReadLine()))/2);
+        }
+    }
+}
