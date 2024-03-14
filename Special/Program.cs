5401cc6b66840306b2588d1a4b67190a1d3df538
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-14 23:59:37 +0000

    Ex4 devidamente comentado

--- Special/Program.cs
+++ Special/Program.cs
@@ -2,8 +2,20 @@
 
 namespace Special
 {
+    /// <summary>
+    /// Programa que pede ao utilizador um número e mostra o seu correspondente
+    /// sobre uma sequência especial
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Percebo porquê que funciona mas não conseguiria fazer este código 
+        /// sozinho, ainda tenho dificuldade em perceber se fiz da maneira 
+        /// correta ou não
+        /// </summary>
+        /// <param name="n">Recebe o número para retornar o seu
+        /// correspondente da sequência</param>
+        /// <returns>(O número - 1) + 2 * (O número - 2)</returns>
         private static int GetSpecial(int n)
         {
             if (n <= 0)
@@ -11,6 +23,11 @@ namespace Special
             else
                 return GetSpecial(n-1) + 2 * GetSpecial(n-2);
         }
+        /// <summary>
+        /// Pergunta ao utilizador que número quer e envia para a função
+        /// GetSpecial para receber o número da sequência correspondente
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.Write("Insert n-th number of the special sequence: ");
